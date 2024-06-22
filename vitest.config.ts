26709cacf0e3e10eb255fc056a58faee8e3fb978
Alexander Vödisch <voedisch.alexander@gmail.com>
2024-06-22 23:39:11 +0200

    chore: Update dependencies in package.json

---
+++ vitest.config.ts
@@ -0,0 +1,40 @@
+import Vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vitest/config'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver, VueUseComponentsResolver } from 'unplugin-vue-components/resolvers'
+
+export default defineConfig({
+  test: {
+    include: ['tests/**/*.test.ts', 'src/**/*.test.ts'],
+    coverage: {
+      provider: 'istanbul',
+    },
+  },
+  plugins: [
+    Vue(),
+    Components({
+      dts: 'src/types/components.d.ts',
+      deep: true,
+      directoryAsNamespace: true,
+      include: [/\.vue$/, /\.vue\?vue/],
+      globalNamespaces: ['components', 'layouts'],
+      dirs: ['src/components', 'src/layouts'],
+      resolvers: [VueUseComponentsResolver(), NaiveUiResolver()],
+    }),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vue-i18n',
+        '@vueuse/head',
+        '@vueuse/core',
+      ],
+      dts: 'src/types/auto-import.d.ts',
+      vueTemplate: true,
+      eslintrc: {
+        enabled: true,
+      },
+    }),
+  ],
+})
