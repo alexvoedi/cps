e5bbe9059c66f9f38018d1c7f8c305f1adde376c
Alexander Vödisch <voedisch.alexander@gmail.com>
2024-08-03 15:17:34 +0200

    refactor: rename suicide king to priority list

--- src/utils/getRandomIndices.ts
+++ src/utils/getRandomIndices.ts
@@ -1,5 +1,4 @@
 export function getRandomIndices<T>(array: T[], count: number): number[] {
-  const indices = Array.from({ length: array.length }, (_, i) => i)
-  const shuffled = indices.toSorted(() => Math.random() - 0.5)
-  return shuffled.slice(0, count)
+  const indices = Array.from({ length: array.length }, (_, i) => i).sort(() => Math.random() - 0.5)
+  return indices.slice(0, count)
 }
