c5df42897a5c33dfb5fe95020b4c8aeb691e8c2f
Alexander Vödisch <voedisch.alexander@gmail.com>
2024-08-03 20:54:41 +0200

    ci: dockerize

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import Inspect from 'vite-plugin-inspect'
 import { NaiveUiResolver, VueUseComponentsResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
-  base: '/cps/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
